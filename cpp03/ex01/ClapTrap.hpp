300c4b4f9dbb98e88455b3444ad79e4eb4edc518
Artur Petoyan <apetoyan@c2r3s3.42yerevan.am>
2025-12-10 21:37:16 +0400

    Ended cpp04

--- cpp03/ex01/ClapTrap.hpp
+++ cpp03/ex01/ClapTrap.hpp
@@ -6,23 +6,23 @@
 
 class ClapTrap
 {
-// protected:
-public:
-	std::string name;
-	int hit;
-	int energy;
-	int damage;
-	ClapTrap(std::string name);
-	ClapTrap(const ClapTrap& cp);
-	ClapTrap& operator=(const ClapTrap& op);
-	~ClapTrap();
-	std::string getName();
-	int getHit() const;
-	int getEnergy() const;
-	int getDamage() const;
-	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
+	protected:
+		std::string name;
+		int hit;
+		int energy;
+		int damage;
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& cp);
+		ClapTrap& operator=(const ClapTrap& op);
+		virtual ~ClapTrap();
+		std::string getName();
+		int getHit() const;
+		int getEnergy() const;
+		int getDamage() const;
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
 };
 
 #endif
